9f6546250b50f5403973149fbff2f385b0fd68e1
Carula <carulagarat@gmail.com>
2024-08-30 17:01:13 +0200

    about

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ProductCatalog from './components/ProductCatalog';
+import About from './About';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/divartsity/" element={<ProductCatalog />} />
-        {/* Add other routes here */}
-      </Routes>
+      <div className="app">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/divartsity">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/divartsity" element={<ProductCatalog />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
