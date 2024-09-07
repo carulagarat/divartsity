b98a4ea66baa50ca3dfbd48d523ac9c89869295d
Carula <carulagarat@gmail.com>
2024-09-07 21:33:50 +0200

    images

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -13,11 +13,11 @@ const ProductDetail = ({ products, addToCart }) => {
   const [isUnpainted, setIsUnpainted] = useState(false);
 
   useEffect(() => {
-    if (product && product.variants.length > 0) {
-      setSelectedVariant(product.variants[0]);
+    if (product && product.variants.length > 0 && !selectedVariant) {
+      setSelectedVariant(product.variants[0]); // Set the first variant as default
     }
-  }, [product]);
-
+  }, [product, selectedVariant]);
+  
   if (!product) {
     return <p>Product not found!</p>;
   }
@@ -85,15 +85,31 @@ const ProductDetail = ({ products, addToCart }) => {
     <div className='page'>
       <div className='content'>
         <div className='row product-detail'>
-          <div className="product-images">
-            <Carousel showArrows={true} autoPlay={true} infiniteLoop={true} showThumbs={false}>
-              {product.images.map((image, index) => (
-                <div key={index}>
-                  <img src={`/divartsity/images/product-images/${product.id}/gallery/${product.id}_${image}.jpg`} alt={`${product.name} image ${index + 1}`} />
-                </div>
-              ))}
-            </Carousel>
-          </div>
+
+        <div className="product-images">
+          <Carousel
+            showArrows={true}
+            autoPlay={false}
+            infiniteLoop={true}
+            showThumbs={false}
+            swipeable={true}
+            emulateTouch={true}
+            showStatus={false}
+          >
+            {Array.from({ length: product.images }, (_, index) => (
+              <div key={index}>
+                <img 
+                  src={`/divartsity/images/product-images/${product.id}/gallery/${product.id}_${index + 1}.jpg`} 
+                  alt={`${product.name} image ${index + 1}`} 
+                />
+              </div>
+            ))}
+          </Carousel>
+        </div>
+
+
+
+
           <div className='product-info column'>
             <h1>{product.name}</h1>
             {product.scienceName ? <h2>{product.scienceName}</h2> : null}
@@ -105,31 +121,37 @@ const ProductDetail = ({ products, addToCart }) => {
           </div>
         </div>
 
-        <div className='filters row product-selection'>
-          <div className='field w190'>
-              <select name="scale" value={selectedVariant?.size || ''} onChange={handleVariantSelect}>
-                {product.variants.map((variant, index) => (
-                  <option key={index} value={variant.size}>
-                    {variant.size}
-                  </option>
-                ))}
-              </select>
-              <label htmlFor='scale'>Scale</label>
-              <FontAwesomeIcon icon={faChevronDown} />
+        <div className='filters row stretch product-selection'>
+
+
+          <div className='column'>
+
+            {selectedVariant && (
+              <div className='img-dimensions'>
+                <img 
+                  src={`/divartsity/images/product-images/${product.id}/${product.id}-dimensions.jpg`} 
+                  alt={`${product.name} dimensions`} 
+                />
+                <span className='width'>{selectedVariant.dimensions.width}cm</span>
+                <span className='height'>{selectedVariant.dimensions.height}cm</span>
+              </div>
+            )}
+
+            <div className='fieldWrapper'>
+              <div className='field'>
+                  <select name="scale" value={selectedVariant?.size || ''} onChange={handleVariantSelect}>
+                    {product.variants.map((variant, index) => (
+                      <option key={index} value={variant.size}>
+                        {variant.size}
+                      </option>
+                    ))}
+                  </select>
+                  <label htmlFor='scale'>Scale</label>
+                  <FontAwesomeIcon icon={faChevronDown} />
+              </div>
+            </div>
+
           </div>
-
-          {selectedVariant && (
-            <p className='dimensions field w190'>
-              <span>Dimensions:</span>
-              <span>{selectedVariant.dimensions.width}</span>
-              <span>x</span>
-              <span>{selectedVariant.dimensions.height}</span>
-              <span>x</span>
-              <span>{selectedVariant.dimensions.depth}</span>
-              <span>cm</span>
-            </p>
-          )}
-
           <div className='field checkbox'>
             <input 
               type='checkbox' 
@@ -139,42 +161,48 @@ const ProductDetail = ({ products, addToCart }) => {
             />
             <label htmlFor='unpainted'>Unpainted</label>
           </div>
-        </div>
+          
+          <div className='column checkout'>
+            {selectedVariant && (
+              <div className='prePrice row fullwidth bottom right'>
+                <div className='row middle'>
 
-        {selectedVariant && (
-          <div className='prePrice row fullwidth middle right'>
-            {isUnpainted ? (
-              <div className='row middle'>
-                <span className='prevPrice'>
-                  {selectedVariant.price}€
-                </span>
-                <span className='offer'>
-                  -{discountPercentage}%
-                </span>
+                  {isUnpainted ? (
+                    <div className='row middle'>
+                      <span className='prevPrice'>
+                        {selectedVariant.price}€
+                      </span>
+                      <span className='offer'>
+                        -{discountPercentage}%
+                      </span>
+                    </div>
+                  ) : null }
+
+                  <p className='price'>{calculateVariantPrice()}€</p>
+                  <span className='times'>x</span>
+                  <div className='field quantity'>
+                    <input
+                      name='quantity'
+                      type='number'
+                      value={quantity}
+                      onChange={handleQuantityChange}
+                      min="1"
+                      />
+                    <label htmlFor='quantity'>Quantity:</label>
+                  </div>
+                </div>
               </div>
-            ) : null }
+            )}
 
-            <p className='price'>{calculateVariantPrice()}€</p>
-            <span className='times'>x</span>
-            <div className='field quantity'>
-              <input
-                name='quantity'
-                type='number'
-                value={quantity}
-                onChange={handleQuantityChange}
-                min="1"
-              />
-              <label htmlFor='quantity'>Quantity:</label>
+            <div className='row fullwidth right'>
+              <div className='row middle right total'>
+                <div className='price'>
+                  <span>Total price:</span> {calculateTotalPrice()}€
+                </div>
+                <button className='primary' onClick={handleAddToCart}>Add to Cart</button>
+              </div>
             </div>
-          </div>
-        )}
 
-        <div className='row fullwidth right'>
-          <div className='row middle total'>
-            <div className='price'>
-              <span>Total price:</span> {calculateTotalPrice()}€
-            </div>
-            <button className='primary' onClick={handleAddToCart}>Add to Cart</button>
           </div>
         </div>
 
